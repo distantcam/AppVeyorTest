0c7006397a0f5890ced36a3125c2e2f24d96c566
Cameron MacFarland <distantcam@gmail.com>
2016-01-09 16:41:52 +0800

    Going back to old webrequest

--- BuildScript/UpdateVersionTask.cs
+++ BuildScript/UpdateVersionTask.cs
@@ -1,24 +1,15 @@
 ﻿using System;
 using System.Net;
-using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Build.Utilities;
 
-public class UpdateVersionTask : Microsoft.Build.Utilities.Task
+public class UpdateVersionTask : Task
 {
     public override bool Execute()
     {
         try
         {
-            AsyncPump.Run(InnerExecute);
-        }
-        catch (AggregateException ae)
-        {
-            ae.Handle(e =>
-            {
-                Log.LogErrorFromException(e);
-                return true;
-            });
+            InnerExecute();
         }
         catch (Exception e)
         {
@@ -28,7 +19,7 @@ public class UpdateVersionTask : Microsoft.Build.Utilities.Task
         return !Log.HasLoggedErrors;
     }
 
-    private async Task InnerExecute()
+    private void InnerExecute()
     {
         if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("APPVEYOR")))
             return;
@@ -37,19 +28,28 @@ public class UpdateVersionTask : Microsoft.Build.Utilities.Task
 
         var restBase = Environment.GetEnvironmentVariable("APPVEYOR_API_URL");
 
-        using (var client = new HttpClient())
+        var request = (HttpWebRequest)WebRequest.Create(restBase + "api/build");
+        request.Method = "PUT";
+
+        Log.LogMessage("AppVeyor PUT {0}api/build", restBase);
+
+        var data = string.Format("{{ \"version\": \"1.0+build{0}\" }}", buildNumber);
+        Log.LogMessage("AppVeyor Content: {0}", data);
+
+        var bytes = Encoding.UTF8.GetBytes(data);
+        request.ContentLength = bytes.Length;
+        request.ContentType = "application/json";
+
+        using (var writeStream = request.GetRequestStream())
         {
-            Log.LogMessage("AppVeyor PUT {0}api/build", restBase);
+            writeStream.Write(bytes, 0, bytes.Length);
+        }
 
-            var data = string.Format("{{ \"version\": \"1.0+build{0}\" }}", buildNumber);
-            Log.LogMessage("AppVeyor Content: {0}", data);
-
-            using (var response = await client.PutAsync(restBase + "api/build", new StringContent(data, Encoding.UTF8, "application/json")).ConfigureAwait(false))
+        using (var response = (HttpWebResponse)request.GetResponse())
+        {
+            if (response.StatusCode != HttpStatusCode.OK && response.StatusCode != HttpStatusCode.NoContent)
             {
-                if (response.StatusCode != HttpStatusCode.OK && response.StatusCode != HttpStatusCode.NoContent)
-                {
-                    Log.LogError("Respose Error {0}", response.StatusCode);
-                }
+                Log.LogError("Respose Error {0}", response.StatusCode);
             }
         }
     }
